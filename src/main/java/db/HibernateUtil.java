04a3d47252e09c7572abbb4e87523300f9bb3620
Vicky Jackson <vicky@digitalstuff.net>
2018-08-31 16:24:07 +0100

    heroku test

--- src/main/java/db/HibernateUtil.java
+++ src/main/java/db/HibernateUtil.java
@@ -3,11 +3,15 @@ package db;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
+
         try {
             // Create the SessionFactory from hibernate.cfg.xml
             return new Configuration().configure().buildSessionFactory();
